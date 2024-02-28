4a1f0d3e58ec54638cd4523ca2646d9af49a9348
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-02-28 12:30:22 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+"""
+def print_fibonacci(length):
+    fibonacci_sequence = [0, 1]
+    for _ in range(length):
+        next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_num)
+    return fibonacci_sequence
+
+print(print_fibonacci(10))
+
+"""
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    
+    for i in range(length):
+        if i == 0 or i == 1:
+            fib_sequence.append(i)
+        else:
+           fib_sequence.append(fib_sequence[i - 2] + fib_sequence[i - 1])   
+    print(fib_sequence)
+        
